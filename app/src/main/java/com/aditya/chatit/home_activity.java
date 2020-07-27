db2aaaa8483d2026f2976c9750bb9091bfbd4cde
adityakamath16 <61751718+adityakamath16@users.noreply.github.com>
2020-07-27 15:03:41 +0530

    Security added to home page, so that only authenticated users can login.

--- app/src/main/java/com/aditya/chatit/home_activity.java
+++ app/src/main/java/com/aditya/chatit/home_activity.java
@@ -2,13 +2,30 @@ package com.aditya.chatit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class home_activity extends AppCompatActivity {
 
+    FirebaseAuth firebaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activity);
+    }
+
+
+    //to confirm weather the user is logged in to the app or not
+    @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() == null)
+        {
+            finish();
+            startActivity(new Intent(this,MainActivity.class));
+        }
     }
 }
