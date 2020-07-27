ddd4ed2ea08467a592f03033bc94353654a40d6c
adityakamath16 <61751718+adityakamath16@users.noreply.github.com>
2020-07-27 23:51:03 +0530

    Added tab layout and certain major home screen changes...

---
+++ app/src/main/java/com/aditya/chatit/ChatitUser.java
@@ -0,0 +1,39 @@
+package com.aditya.chatit;
+
+public class ChatitUser {
+    private String id, username,imageURL;
+
+    public ChatitUser(String id, String username, String imageURL) {
+        this.id = id;
+        this.username = username;
+        this.imageURL = imageURL;
+    }
+
+    public ChatitUser()
+    {
+
+    }
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(String imageURL) {
+        this.imageURL = imageURL;
+    }
+}
--- app/src/main/java/com/aditya/chatit/home_activity.java
+++ app/src/main/java/com/aditya/chatit/home_activity.java
@@ -1,21 +1,121 @@
 package com.aditya.chatit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.Toast;
 
+
+import com.aditya.chatit.Fragments.ChatsFragment;
+import com.aditya.chatit.Fragments.UsersFragment;
+import com.bumptech.glide.Glide;
+import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class home_activity extends AppCompatActivity {
 
+    CircleImageView profile_image;
+    TextView username_fire;
+
+    FirebaseUser firebaseUser;
+    DatabaseReference reference;
     FirebaseAuth firebaseAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_activity);
+
+        //for Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("");
+
+        profile_image = findViewById(R.id.profile_image);
+        username_fire = findViewById(R.id.username);
+
+        firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users").child(firebaseUser.getUid());
+
+
+        //for retreiving user name
+            reference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+
+                    ChatitUser user = dataSnapshot.getValue(ChatitUser.class);
+                    username_fire.setText(user.getUsername());
+                    if (user.getImageURL().equals("default")) {
+                        profile_image.setImageResource(R.mipmap.ic_launcher);
+                    } else {
+                        Glide.with(home_activity.this).load(user.getImageURL()).into(profile_image);
+
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+            // for the bottom viewpager and tablayout
+        TabLayout tabLayout = findViewById(R.id.home_tab_layout);
+        ViewPager viewPager =findViewById(R.id.home_viewpager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        viewPagerAdapter.addFragment(new ChatsFragment(),"Chats");
+        viewPagerAdapter.addFragment(new UsersFragment(),"Users");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
     }
 
+    //for top right menu
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId())
+        {
+            case R.id.logout:
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(home_activity.this, MainActivity.class));
+                finish();
+                return true;
+        }
+        return false;
+    }
 
     //to confirm weather the user is logged in to the app or not
     @Override
@@ -28,4 +128,42 @@ public class home_activity extends AppCompatActivity {
             startActivity(new Intent(this,MainActivity.class));
         }
     }
+
+    class ViewPagerAdapter extends FragmentPagerAdapter{
+
+        private ArrayList<Fragment> fragments;
+        private ArrayList<String> titles;
+        ViewPagerAdapter(FragmentManager fm)
+        {
+            super(fm);
+            this.fragments = new ArrayList<>();
+            this.titles = new ArrayList<>();
+        }
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return fragments.size();
+        }
+
+        public void addFragment(Fragment fragment, String title)
+        {
+            fragments.add(fragment);
+            titles.add(title);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return titles.get(position);
+        }
+    }
+
+
+
+
 }
