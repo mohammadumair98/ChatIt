6dbd978047d97fec8d2dea3f6c364ed7e03b57e6
adityakamath16 <61751718+adityakamath16@users.noreply.github.com>
2020-07-28 13:32:11 +0530

    COMMIT 19:-  Users can now send message with one to one user

--- app/src/main/java/com/aditya/chatit/MessageActivity.java
+++ app/src/main/java/com/aditya/chatit/MessageActivity.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
+import android.media.MediaDataSource;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -28,6 +35,11 @@ public class MessageActivity extends AppCompatActivity {
     DatabaseReference reference;
 
     Intent intent;
+
+    //for sending messages
+    ImageButton btn_send;
+    EditText text_send;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +57,9 @@ public class MessageActivity extends AppCompatActivity {
             }
         });
 
+        //for the message sending part
+        btn_send = findViewById(R.id.btn_message_send);
+        text_send = findViewById(R.id.text_send_message);
 
         profile_image = findViewById(R.id.profile_image_message_activity);
         username = findViewById(R.id.username_message_activity);
@@ -72,5 +87,33 @@ public class MessageActivity extends AppCompatActivity {
 
             }
         });
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+        //when the user writes a message and clicks on the send button
+        btn_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String msg= text_send.getText().toString();
+                if(!msg.equals(""))
+                {
+                    sendMessage(fuser.getUid(),userid,msg);
+                }
+                else
+                {
+                    Toast.makeText(MessageActivity.this, "You cannot send an empty message", Toast.LENGTH_SHORT).show();
+                }
+                text_send.setText("");
+            }
+        });
+    }
+
+    private void sendMessage(String sender, String receiver, String message)
+    {
+        DatabaseReference reference1  = FirebaseDatabase.getInstance().getReference();
+        HashMap<String, Object> hashMap = new HashMap<>();
+        hashMap.put("sender",sender);
+        hashMap.put("receiver", receiver);
+        hashMap.put("message", message);
+
+        reference1.child("chats").push().setValue(hashMap);
     }
 }
